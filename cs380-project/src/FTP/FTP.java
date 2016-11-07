b730801f735a1f6438ddfb060962063a40b312c6
andre <andre@DESKTOP-BHBOLO1>
2016-11-06 16:25:06 -0800

    Added the two projects into a single one. A terminal value of server will launch the server while the value client will launch the client

--- cs380-project/src/FTP/FTP.java
+++ cs380-project/src/FTP/FTP.java
@@ -4,13 +4,41 @@
  * and open the template in the editor.
  */
 package FTP;
+import client.Client;
+import server.Server;
 /**
  *
- * @author andre
+ * @author andrew
  */
 public class FTP {
     public static void main(String[] args)
     {
-
-    }   
+        FTP ftp = new FTP();
+        if(args.length > 0){
+            if(args[0].equals("server")){
+                ftp.server();
+                return;
+            }
+            else if(args[0].equals("client")){ 
+                ftp.client();
+                return;
+            }
+        }
+        help();
+    }  
+    private static void help(){
+        System.out.println("Usage: \"client\" or \"server\"");
+    }
+    private void client(){
+        Client cln = new Client();
+        cln.start("localhost", 8888);
+        cln.login();
+        cln.menu();
+    }
+    private void server(){
+        Server srv = new Server();
+        srv.start(8888);
+        srv.authenticate();
+        srv.menu();
+    }
 }
